0c26e1ee577937ca27cee33dd725b4395c05869b
Sam Tregea <sdtregea@gmail.com>
2025-05-31 14:51:45 -0400

    Add initial spring project

---
+++ src/main/java/com/samueltregea/flight_recommendation_service/FlightRecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.samueltregea.flight_recommendation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightRecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightRecommendationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samueltregea/flight_recommendation_service/FlightRecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samueltregea.flight_recommendation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightRecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
