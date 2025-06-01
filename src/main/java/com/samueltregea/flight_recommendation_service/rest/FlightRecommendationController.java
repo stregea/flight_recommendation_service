733414fec1478358539f8526ed32374f6b6b7ea6
Sam Tregea <sdtregea@gmail.com>
2025-05-31 21:53:31 -0400

    Create the initial FlightRecommendationController

---
+++ src/main/java/com/samueltregea/flight_recommendation_service/rest/FlightRecommendationController.java
@@ -0,0 +1,35 @@
+package com.samueltregea.flight_recommendation_service.rest;
+
+
+import com.samueltregea.flight_recommendation_service.domain.aviationstack.Flight;
+import com.samueltregea.flight_recommendation_service.service.AviationStackService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class FlightRecommendationController {
+    private static final Logger LOG = LogManager.getLogger(FlightRecommendationController.class);
+
+    @Autowired
+    private final AviationStackService aviationStackService;
+
+    @Autowired
+    public FlightRecommendationController(AviationStackService aviationStackService) {
+        this.aviationStackService = aviationStackService;
+    }
+
+    @GetMapping("/api/flights")
+    public List<Flight> getFlights(@RequestParam String departure, @RequestParam String arrival) {
+        // Mock data for demonstration
+        List<Flight> flights = null;
+        LOG.info("-> getFlights");
+        flights = aviationStackService.getFlights(departure, arrival);
+        LOG.info("<- getFlights");
+        return flights;
+    }}
