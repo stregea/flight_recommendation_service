8dc17204ba619ea5db52c8425c1eed6e4a6a4e6a
Sam Tregea <sdtregea@gmail.com>
2025-05-31 21:53:50 -0400

    Add a unit test for the Flight object

---
+++ src/test/java/com/samueltregea/flight_recommendation_service/domain/FlightTest.java
@@ -0,0 +1,28 @@
+package com.samueltregea.flight_recommendation_service.domain;
+
+import com.samueltregea.flight_recommendation_service.domain.aviationstack.Flight;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class FlightTest {
+    Flight underTest;
+
+    @BeforeEach
+    public void setup() {}
+
+    /**
+     * Test the standard Flight record and determine non-null values.
+     */
+    @Test
+    public void testFlightRecord() {
+//        underTest = new Flight("Delta", "ROC", "ATL", "$400");
+//
+//        assertNotNull(underTest);
+//        assertNotNull(underTest.airline());
+//        assertNotNull(underTest.departure());
+//        assertNotNull(underTest.arrival());
+//        assertNotNull(underTest.price());
+    }
+}
