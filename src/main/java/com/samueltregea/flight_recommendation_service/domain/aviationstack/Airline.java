2c920fd25c8032363f251d6c5d6fe6c344eb5228
Sam Tregea <sdtregea@gmail.com>
2025-05-31 23:37:16 -0400

    Create the Airline record

---
+++ src/main/java/com/samueltregea/flight_recommendation_service/domain/aviationstack/Airline.java
@@ -0,0 +1,4 @@
+package com.samueltregea.flight_recommendation_service.domain.aviationstack;
+
+public record Airline(String name, String iata, String icao) {
+}
---
+++ src/test/java/com/samueltregea/flight_recommendation_service/domain/AirlineTest.java
@@ -0,0 +1,29 @@
+package com.samueltregea.flight_recommendation_service.domain;
+
+import com.samueltregea.flight_recommendation_service.domain.aviationstack.Airline;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class AirlineTest {
+
+
+    @BeforeEach
+    public void setup() {}
+
+    @Test
+    public void testAirline() {
+        String name = "American Airlines";
+        String iata = "AA";
+        String icao = "AAL";
+
+        Airline airline = new Airline(name, iata, icao);
+
+        assertNotNull(airline);
+        assertEquals(name, airline.name());
+        assertEquals(iata, airline.iata());
+        assertEquals(icao, airline.icao());
+    }
+}
